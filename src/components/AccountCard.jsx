5f6a7de353b8d2f669206968025a04bc9790810e
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-22 15:04:36 -0600

    refactored using useContext hook in AccountCard component

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Card, Image } from 'semantic-ui-react'
-import { AccountConsumer } from '../providers/AccountProvider'
+import { AccountContext } from '../providers/AccountProvider'
 
-const AccountCard = () => (
-  <AccountConsumer> 
-    { value => (
+const AccountCard = () => {
+  const {dateJoined, avatar, firstName, lastName, username, email} = useContext(AccountContext);
+  return(
     <Card>
-      <Image src={`${value.avatar}`} wrapped ui={false} />
+      <Image src={`${avatar}`} wrapped ui={false} />
       <Card.Content>
-        <Card.Header> Your Profile Page</Card.Header>
+        <Card.Header> {username}</Card.Header>
         <Card.Meta>
-        <span className='date'> Date Joined: {value.dateJoined} </span>
+        <span className='date'> Date Joined: {dateJoined} </span>
         </Card.Meta>
       </Card.Content>
       <Card.Content>
-        <h3> First Name:{value.firstName} </h3>
-        <h3> Last Name: {value.lastName}  </h3>
-        <h3> Email:{value.email}  </h3>
+        <h3> First Name:{firstName} </h3>
+        <h3> Last Name: {lastName}  </h3>
+        <h3> Email:{email}  </h3>
       </Card.Content>
     </Card>
 
-    )}
-  </AccountConsumer>
-)
+)}
 
 export default AccountCard
--- src/providers/AccountProvider.jsx
+++ src/providers/AccountProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AccountContext = React.createContext();
+export const AccountContext = React.createContext();
 
 export const AccountConsumer = AccountContext.Consumer;
 
